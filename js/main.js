9208ad9b5a7fa39c6e3b5a7c74e330cf92c566c9
Janderson-Mota <jandersonmota2407@gmail.com>
2024-12-24 00:18:43 -0300

    atualização 48

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ menuIcon.addEventListener('click',()=>{
 if(ul.classList.contains('ativo')){
  ul.classList.remove('ativo');
 document.querySelector('.menu-icon img').src = '../img/menu.png';
+document.querySelector('.logo img').src = '../img/logo-icone.png';
 
 
 }
@@ -14,6 +15,7 @@ document.querySelector('.menu-icon img').src = '../img/menu.png';
 else{
 ul.classList.add('ativo');
 document.querySelector('.menu-icon img').src = '../img/close.png';
+document.querySelector('.logo img').src = '../img/logo-icone1.png';
 }
 
 }) 
