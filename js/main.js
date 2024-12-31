98848a5aaa4f649b15f6312d40c837b09a971c64
Janderson-Mota <jandersonmota2407@gmail.com>
2024-12-30 21:06:31 -0300

    atualização 54

--- js/main.js
+++ js/main.js
@@ -1,23 +1,15 @@
-var menuIcon = document.querySelector ('.menu-icon');
+var menuIcon = document.querySelector(".menu-icon");
 
-var ul = document.querySelector('.ul');
+var ul = document.querySelector(".ul");
 
-menuIcon.addEventListener('click',()=>{
-   
-if(ul.classList.contains('ativo')){
- ul.classList.remove('ativo');
-document.querySelector('.menu-icon img').src = '../img/menu.png';
-document.querySelector('.logo img').src = '../img/logo-icone.png';
-
-
-}
-
-else{
-ul.classList.add('ativo');
-document.querySelector('.menu-icon img').src = '../img/close.png';
-document.querySelector('.logo img').src = '../img/logo-icone1.png';
-}
-
-}) 
-
-
+menuIcon.addEventListener("click", () => {
+  if (ul.classList.contains("ativo")) {
+    ul.classList.remove("ativo");
+    document.querySelector(".menu-icon img").src = "../img/menu.png";
+    document.querySelector(".logo img").src = "../img/logo-icone.png";
+  } else {
+    ul.classList.add("ativo");
+    document.querySelector(".menu-icon img").src = "../img/close.png";
+    document.querySelector(".logo img").src = "../img/logo-icone1.png";
+  }
+});
